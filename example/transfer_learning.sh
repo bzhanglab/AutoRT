4b190028bce075ae0190d7cc49e8b9b57bc7d36a
Bo Wen <wenbostar@gmail.com>
2021-10-19 14:56:37 -0500

    Fixed model path in example scripts

--- example/transfer_learning.sh
+++ example/transfer_learning.sh
@@ -1,5 +1,5 @@
 ## training
-python ../autort.py train -i data/28CPTAC_COprospective_W_VU_20150810_05CO037_f01_normal_train.tsv -o tf_model/ -e 40 -b 64 -u m -m ../models/base_models_PXD006109/model.json -rlr -n 10
+python ../autort.py train -i data/28CPTAC_COprospective_W_VU_20150810_05CO037_f01_normal_train.tsv -o tf_model/ -e 40 -b 64 -u m -m ../models/general_base_model/model.json -rlr -n 10
 
 ## prediction
 python ../autort.py predict -t data/28CPTAC_COprospective_W_VU_20150810_05CO037_f01_normal_test.tsv -s tf_model/model.json -o tf_prediction/ -p test
